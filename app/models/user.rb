d366441ea6e9e7ef096eb351cd76d5d4e5eb6cd6
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-07-25 07:12:01 +0900

    いいね機能など更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
