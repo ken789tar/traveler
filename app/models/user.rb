e84bca9da1f2a24512ac2abc11413c3dbd3b22d0
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-07-28 07:55:45 +0900

    マイページ編集機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  attachment :profile_image
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
