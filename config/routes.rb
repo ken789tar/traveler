2428a47a9046bdec461e5cbba1a0b6b6f6bdf44a
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-07-30 07:19:59 +0900

    コントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :users, only:[:index, :show, :edit, :update] do
-    menber do
+    member do
       get :follows, :followers
     end
     resource :relationships, only: [:create, :destroy]
@@ -11,6 +10,7 @@ Rails.application.routes.draw do
     resources :comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
   end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
