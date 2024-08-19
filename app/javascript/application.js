092a0f34c281825778e8d5bb04d111fe21be29ee
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-08-19 12:51:24 +0900

    評価機能

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 window. $ = window.jQuery = require('jquery');
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+    let raty = new Raty(elem,opt)
+    raty.init();
+    return raty;
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_16_230807) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_19_030324) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_16_230807) do
     t.string "image_id"
     t.integer "user_id"
     t.integer "status", default: 0, null: false
+    t.string "star"
   end
 
   create_table "read_counts", force: :cascade do |t|
