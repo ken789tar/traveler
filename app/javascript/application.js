548fc4ecf0d21d7614ea3db519516a5f2c1e1457
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-08-22 06:36:33 +0900

    評価機能

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,14 +4,16 @@
 //= require bootstrap
 import "@hotwired/turbo-rails"
 import "controllers"
-window. $ = window.jQuery = require('jquery');
-import Raty from "raty.js"
+import $ from "jquery"
+import Raty from "./raty.js"
 window.raty = function(elem,opt) {
-    let raty = new Raty(elem,opt);
-    raty.init();
-    return raty;
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
 }
+
 $(document).ready(function() {
     $('#star').raty();
   });
-  
+
+import moduleName from './module.js'
