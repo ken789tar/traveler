367ebddad5890c3ad1c516cf777bee3f788e70dc
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-08-25 07:31:02 +0900

    DM機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_22_215815) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_24_222600) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_22_215815) do
   create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
